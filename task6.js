9de92938127e85ce86a93fa2cf4bf86da86218d1
Debugger-ops <vivek9to5@gmail.com>
2026-07-22 12:46:02 +0530

    Add BMI calculator

--- task6.js
+++ task6.js
@@ -1,3 +1,45 @@
-var a = 32;
-a =43;
-console,log(a);
+// question 1
+// let a =-90
+
+// if(a < 0) {
+//    console.log("negative");
+// }else{
+//    console.log("positive");
+// };
+
+//question 2
+// let a = 90
+// if(a%2 === 0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+
+//question 3
+// let a = 5
+// let b = 8
+// if(a>b){
+//     console.log("a is greater")
+// }else{
+//     console.log("b is greater")
+// }
+
+//question 4
+// let age = 5;
+
+// if(age<12){
+//     console.log("ticket price is 5")
+// }else if(age <18 && age>12){
+//     console.log("ticket price is 10")
+// }else if(age>18 && age<60){
+//     console.log("ticket price is 15")
+// }else{
+//     console.log("ticket price is 10")
+// }
+
+//question 5
+let bodymass =70;
+height = 1.74;
+let bmi =0;
+bmi = bodymass/(height*height);
+console.log(bmi)
