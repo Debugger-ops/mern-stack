7b4a7bb8d4e74100ab68838ed7432c45b911bab6
Debugger-ops <vivek9to5@gmail.com>
2026-07-25 21:05:40 +0530

    add task html and js 7

---
+++ task7.js
@@ -0,0 +1,13 @@
+let num = 4;
+console.log("5" + num);
+let num2 =0;
+console.log(num2 + "5");
+console.log(true + num);
+let checking = null + 3;
+console.log(5 == '10');
+console.log(34 === '34');
+
+let arr= ["apple", "banana", "mango"]; 
+let string = "apple, banana, mango";
+console.log(arr);
+console.log(string);    
