548566900d8ce9d0788ef8f5302cf2f556f7b850
Debugger-ops <vivek9to5@gmail.com>
2026-07-27 12:45:26 +0530

    added task8.js

---
+++ task8.js
@@ -0,0 +1,40 @@
+// // x = "25";
+// // function abc(){
+// //     return abc;
+// // }
+// // abc();
+// let string  = "vievek";
+// let string2 = "vivek";
+// let str = 89943;
+// let palin = "jot"
+// let check = palin.split(".").reverse().join(".");
+// console.log(check);
+// let strings = "vivek";
+// let str =3948;
+// string.includes()\
+// console.log(string.includes('h'))
+// console.log(string.slice( 3 , 7))
+// console.log(string.toUpperCase())
+// console.log(string.replace('mah', 'nit'))
+// anagram
+// write a progrma to check anagram in string?
+
+// let a =  "silent";
+// let b = "lisden";
+// function isAnagram(a,b){
+//     if(a.length !== b.length){
+//         return false;
+//     }
+//     return a.split("").sort().join("") === b.split("").sort().join("");
+    
+
+    
+// }
+// console.log(isAnagram(a,b));
+
+// console.log(string.replace('man','dog'))
+
+function checksomething(...string){
+    return `hello $(a)`;
+}
+console.log(checksomething("india"));
