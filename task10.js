d87df00493d208029c48d9390fdaf1a85d789d7d
Debugger-ops <vivek9to5@gmail.com>
2026-07-31 11:11:48 +0530

    add task11.js

--- task10.js
+++ task10.js
@@ -13,8 +13,24 @@
 // console.log(sum(1))
 // console.log(sum(1,6,7,7,8,9,9))
 
-const arr = ["A", "B", "T","E"]
-arr.unshift("FSDL", true, 34)
-console.log(arr)
-arr.shift()
-console.log(arr)
+// const arr = ["A", "B", "T","E"]
+// // arr = ["P", "N", "R","D"]
+// arr[0] ="mah"
+// arr.unshift("FSDL", true, 34)
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+// arr.splice(2,0," na", true, 34)
+// arr.splice(1,2)
+
+// const arr = Array.from([2,3,4,5], x=>x * x);
+// const numbervalue = [4,8,554,6,8,4]
+// numbervalue.toReversed.sort()
+// console.log(numbervalue)
+
+const numbervalue = [43, 84, 554, 64, 28, 4];
+
+const result = numbervalue.toReversed().sort((a, b) => b - a);
+
+console.log(result);      
+
---
+++ task11.js
@@ -0,0 +1,6 @@
+const arr = [2,2,3,5]
+const arr1 = arr.map((a)=> a * a);
+console.log(arr1)
+const arr2 = arr.filter((num)=> num%2 === 0 )
+console.log(arr2)
+arr2.reduce((1),0)
